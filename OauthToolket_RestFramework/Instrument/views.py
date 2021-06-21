42944143e9603efd4ce70fcddba7f32b73cd6e47
Phan Thế Tuệ <68698678+ptt2309dln0105@users.noreply.github.com>
2021-06-21 23:40:59 +0700

    Finish Set scope for function in class ViewSet

--- OauthToolket_RestFramework/Instrument/views.py
+++ OauthToolket_RestFramework/Instrument/views.py
@@ -1,9 +1,12 @@
+from django.http import JsonResponse
 from oauth2_provider.contrib.rest_framework import TokenMatchesOASRequirements, OAuth2Authentication
+from oauth2_provider.decorators import protected_resource
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
 
 from .models import Instrument
 from .serializers import InstrumentSerializer
+from api_base.permissions import TokenRequirements
 
 
 # Test Using ViewSet - using TokenMatchesOASRequirements
@@ -11,10 +14,35 @@ class InstrumentViewSet(viewsets.ModelViewSet):
     authentication_classes = [OAuth2Authentication]
     queryset = Instrument.objects.all()
     serializer_class = InstrumentSerializer
-    permission_classes = [TokenMatchesOASRequirements]
+    permission_classes = [TokenRequirements]
     required_alternate_scopes = {
         "GET": [["instruments:read"], ["instruments:response"]],
         "POST": [["instruments:write"]],
         "PUT": [["instruments:write"]],
         "DELETE": [["instruments:write"]],
     }
+
+    @action(methods=["get"], detail=False)
+    def response_test(self, request):
+
+        token = request.auth
+        required_scopes = {
+            "GET": [["instruments:response"]]
+        }
+        if not token:
+            data = {
+                'msg': "No scope provide"
+            }
+            return JsonResponse(data)
+
+        for alt in required_scopes["GET"]:
+            if token.is_valid(alt):
+                data = {
+                    'msg': "Success"
+                }
+                return JsonResponse(data)
+
+        data = {
+            'msg': "You do not have this permission"
+        }
+        return JsonResponse(data)
