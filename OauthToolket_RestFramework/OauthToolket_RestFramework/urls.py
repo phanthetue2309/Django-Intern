b5e8f520dc02af6f24b91b8b73c0112e3394018f
KhoaDautay <cuonglieu3256@gmail.com>
2021-06-22 18:24:25 +0700

    Create Scopes and Resource

--- OauthToolket_RestFramework/OauthToolket_RestFramework/urls.py
+++ OauthToolket_RestFramework/OauthToolket_RestFramework/urls.py
@@ -1,20 +1,15 @@
 from django.urls import path, include
 from django.contrib import admin
-from Album.views import *
-from Instrument.views import *
-from rest_framework import routers
-
+from . import api_router
 
 admin.autodiscover()
-router = routers.DefaultRouter()
-router.register('album-test', AlbumViewSet, basename='album_test')
-router.register('instrument-test', InstrumentViewSet, basename='instrument_test')
+
 
 # Setup the URLs and include login URLs for the browsable API.
 urlpatterns = [
+    path('api/v1/', include(api_router)),
     path('admin/', admin.site.urls),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('api/', include(router.urls)),
-    path('authentication/', include('accounts.urls')),
+    # path('authentication/', include('accounts.urls')),
     # ...
 ]
